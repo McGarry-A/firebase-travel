2dfd9e74d71baaffc78e618023fd640e7d8eeb64
Ahmed <atomcgarry@gmail.com>
2022-05-08 00:04:20 +0100

    hover states for tailwind comp

--- src/components/chefCard/ChefCard.js
+++ src/components/chefCard/ChefCard.js
@@ -5,7 +5,7 @@ const ChefCard = () => (
       <img
         className="mb-3 w-20 h-20 rounded-full shadow-lg object-cover"
         src="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="
-        alt="chef image"
+        alt="chef"
       />
       <h3 className="my-2">Gordon Ramsey</h3>
       <p className="text-sm text-gray-400">Goated Chef</p>
